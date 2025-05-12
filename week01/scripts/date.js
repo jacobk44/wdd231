cb9f366f6624600d35929951b97c70b35e360613
jacob amoah <>
2025-05-12 20:09:22 +0000

    add code

--- week01/scripts/date.js
+++ week01/scripts/date.js
@@ -6,13 +6,8 @@ const recentYear = new Date().getFullYear();
 footerElement.innerHTML = `<span>© ${recentYear},❤  Jacob Amoah  ❤  Greater Accra.</span>`
 
 
-
-
-
 function setLastModified() {
 const modifiedDate = document.lastModified;
 document.querySelector("#lastModified").textContent = `Last Modified: ${modifiedDate}`;
 }
-
-
 setLastModified();
