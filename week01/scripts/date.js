d8a75d2b6ad8b4fcb6bd4e42548547ef28335549
jacob amoah <>
2025-05-12 05:55:24 +0000

    add code

---
+++ week01/scripts/date.js
@@ -0,0 +1,7 @@
+
+const footerElement = document.getElementById("year");
+
+const recentYear = new Date().getFullYear();
+
+footerElement.innerHTML = `<span>© ${recentYear},❤  Jacob Amoah  ❤  Greater Accra.</span>`
+
