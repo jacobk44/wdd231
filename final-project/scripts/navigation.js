011eb5a78f8d0422659ca78fe0a20602876211d6
jacobkwartengamoah@gmail.com <>
2025-06-10 02:40:10 +0000

    add code

---
+++ final-project/scripts/navigation.js
@@ -0,0 +1,7 @@
+const menuButton = document.querySelector("#myButton");
+const navElement = document.querySelector("#animateme");
+menuButton.addEventListener("click", () => {
+    navElement.classList.toggle("open");
+    menuButton.classList.toggle("open");
+});
+
