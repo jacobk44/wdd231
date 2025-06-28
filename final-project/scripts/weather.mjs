67a7473476d6e9bb3e8094677a474fbc99df8761
jacobkwartengamoah@gmail.com <>
2025-06-28 17:09:47 +0000

    addde code

--- final-project/scripts/weather.mjs
+++ final-project/scripts/weather.mjs
@@ -19,7 +19,7 @@ function formatTime(unixTime) {
     hour: '2-digit',
     minute: '2-digit',
     hour12: true,
-    timeZone: 'America/Los_Angeles'
+    timeZone: 'Africa/Accra'
   }).toLowerCase();
 }
 
